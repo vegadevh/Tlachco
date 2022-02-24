230848a3c59229fc66c2834fb19644fbfe75c5e9
vegadevh <00148816@uca.edu.sv>
2022-02-23 19:34:07 -0600

    Estado inicial de la aplicacion

---
+++ src/main/java/com/tlachco/observatoriodigital/TlachcoApplication.java
@@ -0,0 +1,13 @@
+package com.tlachco.observatoriodigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TlachcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TlachcoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tlachco/observatoriodigital/TlachcoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tlachco.observatoriodigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TlachcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
