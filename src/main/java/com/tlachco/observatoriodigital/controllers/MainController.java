ae802673b1421464d8688676948ed4e173703e4c
vegadevh <00148816@uca.edu.sv>
2022-04-29 10:44:06 -0600

    Cambios en distribucion de controladores

--- src/main/java/com/tlachco/observatoriodigital/controllers/MainController.java
+++ src/main/java/com/tlachco/observatoriodigital/controllers/MainController.java
@@ -1,9 +1,40 @@
 package com.tlachco.observatoriodigital.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 	
+	@RequestMapping("/noticias")
+	public String noticias(Model model) {
 
+		return "noticias";
+	}
+	
+	@RequestMapping("/articulos")
+	public String articulos(Model model) {
+
+		return "articulos";
+	}
+	
+	@RequestMapping("/videos")
+	public String videos(Model model) {
+
+		return "videos";
+	}
+	
+	@RequestMapping("/infografias")
+	public String infografias(Model model) {
+
+		return "infografias";
+	}
+	
+	@RequestMapping("/sobre-nosotros")
+	public String sobreNosotros(Model model) {
+
+		return "sobre-nosotros";
+	}
+	
 }
