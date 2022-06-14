04bc970ce7fc1a29a84d0668541da36af7054aa8
vegadevh <00148816@uca.edu.sv>
2022-06-14 00:24:03 -0600

    Edicion de usuario

--- Tlachco Project/src/main/java/com/tlachco/observatoriodigital/controllers/GeneralController.java
+++ Tlachco Project/src/main/java/com/tlachco/observatoriodigital/controllers/GeneralController.java
@@ -2,20 +2,31 @@ package com.tlachco.observatoriodigital.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.tlachco.observatoriodigital.domains.Usuario;
 import com.tlachco.observatoriodigital.dto.PublicacionesDTO;
 import com.tlachco.observatoriodigital.services.IPublicacionService;
+import com.tlachco.observatoriodigital.services.IUsuarioService;
 
 @Controller
-@RequestMapping("/general")
 public class GeneralController {
 	
 	@Autowired
 	public IPublicacionService publicacionService;
+	
+	@Autowired
+	public IUsuarioService usuarioService;
 
 	
 	@RequestMapping("/busquedaPublicaciones")
@@ -35,4 +46,60 @@ public class GeneralController {
 
 		return "busqueda";
 	}
+	
+	@RequestMapping("/perfil/{username}")
+	public String perfilDeUsuario(@PathVariable("username") String username, Model model) {
+		
+		Usuario usuario = usuarioService.findOne(username);
+		
+		System.out.println(usuario.getNombre() + "EN /perfil/{username}");
+
+		model.addAttribute("usuario",usuario);
+		return "perfil";
+				
+	}
+	
+	@RequestMapping("/perfil/{username}/pass")
+	public String editarPass(@PathVariable("username") String username, Model model) {
+		
+		Usuario usuario = usuarioService.findOne(username);
+		
+		model.addAttribute("usuario",usuario);
+		return "editarPass";
+				
+	}
+	
+	@RequestMapping("/perfil/{username}/validar-pass")
+	public String validarPass(@Valid @ModelAttribute Usuario usuario, BindingResult result, @RequestParam("password") String password, @RequestParam("valid_password") String valid_password,  Model model) {
+		
+		System.out.println(password + " " + valid_password);
+		
+		if (result.hasErrors()) {
+			model.addAttribute("usuario",usuario);
+			return "editarPass";
+		}
+		else if(!password.equals(valid_password)){
+			model.addAttribute("usuario",usuario);
+			return "editarPass";
+			
+		}
+		else {
+			
+			BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+			String encodedPassword = passwordEncoder.encode(usuario.getPassword());
+			
+			usuario.setPassword(encodedPassword);
+			
+			try {
+				usuarioService.save(usuario);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+			
+			model.addAttribute("umensaje","Su contraseña fue cambiada con éxito.");
+			return "perfil";
+		}
+				
+	}
+	
 }
